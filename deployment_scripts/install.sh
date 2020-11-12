f6e9b15e4541f5b18eda32be94cf79b1b3310911
nbarlowATI <nbarlow@turing.ac.uk>
2020-11-12 13:50:07 +0000

    more messing about with conda/bash

--- deployment_scripts/install.sh
+++ deployment_scripts/install.sh
@@ -18,10 +18,10 @@ set +o allexport
 # install miniconda
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p /home/${1}/miniconda
-export PATH=$PATH:/home/${1}/miniconda/bin
+export PATH=/home/${1}/miniconda/bin:$PATH
 echo "about to do conda init bash"
 /home/${1}/miniconda/bin/conda init bash
-#source ~/.bashrc
+source ~/.bashrc
 
 # clone the aml-jupyterhub repo and create the conda environment
 git clone https://github.com/informatics-lab/aml-jupyterhub
