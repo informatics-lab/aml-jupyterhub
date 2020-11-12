4453f37b14d02fe8843b6383520717073bfa57c4
nbarlowATI <nbarlow@turing.ac.uk>
2020-11-12 10:16:49 +0000

    WIP more tests

--- deployment_scripts/install.sh
+++ deployment_scripts/install.sh
@@ -1,6 +1,7 @@
 # !/bin/bash
 
-echo ${1} > tenantId.txt
+echo ${1} > /home/aml-jupyterhub-admin/tenantId.txt
+cd /home/aml-jupyterhub-admin
 git clone https://github.com/informatics-lab/aml-jupyterhub
 
 #curl https://raw.githubusercontent.com/jupyterhub/the-littlest-jupyterhub/master/bootstrap/bootstrap.py \
