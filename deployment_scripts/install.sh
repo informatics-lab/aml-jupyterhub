e68767e5807558bae80f2cc3666bc49e93c0bfca
nbarlowATI <nbarlow@turing.ac.uk>
2020-11-30 16:58:04 +0000

    remove some debug output

--- deployment_scripts/install.sh
+++ deployment_scripts/install.sh
@@ -1,7 +1,20 @@
 # !/bin/bash
 
-# This script will be called during the deployment process with the following args:
-# vm-admin-username  subscription_id  location  tenant_id  client_id  client_secret dns_prefix run_script
+# This script will be called during the deployment process with the
+# following args in order:
+# 1) vm-admin-username
+# 2) subscription_id
+# 3) location
+# 4) tenant_id
+# 5) client_id
+# 6) client_secret
+# 7) dns_prefix
+# 8) run_script
+
+# The script is run as root, but will install things to the home
+# area of the admin user specified in the deployment.
+# At the end, the 'run.sh' script will be run as that user.
+###################################################################
 
 # install jq so we can query json output
 apt update; apt install jq
@@ -9,10 +22,12 @@ apt update; apt install jq
 # install the Azure CLI
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
-
-echo " ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8}" > /home/aml-jupyterhub-admin/vartest.txt
+# Set a few variables here for clarity
+USERNAME=${1}
 HOST="${7}.${3}.cloudapp.azure.com"
-cd /home/${1}
+
+# Change to the user's home directory
+cd /home/${USERNAME}
 
 # redirect port 443 (https) to port 8000
 iptables -t nat -I PREROUTING -p tcp --dport 443 -j REDIRECT --to-ports 8000
@@ -23,11 +38,8 @@ iptables -L -t nat > iptables.txt
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p /home/${1}/miniconda
 
-#export PATH=/home/${1}/miniconda/bin:$PATH
-echo "about to do conda init bash"
-/home/${1}/miniconda/bin/conda init bash
-. /home/${1}/miniconda/etc/profile.d/conda.sh
-echo $PATH > path1.txt
+/home/${USERNAME}/miniconda/bin/conda init bash
+. /home/${USERNAME}/miniconda/etc/profile.d/conda.sh
 
 # clone the aml-jupyterhub repo
 git clone https://github.com/informatics-lab/aml-jupyterhub
@@ -38,12 +50,11 @@ ln -t /usr/local/bin/ /usr/local/etc/certbot/letsencrypt-auto
 
 letsencrypt-auto certonly --standalone --agree-tos -m email@example.co.uk --no-eff-email -d $HOST
 # Change permission to makekey and cert visible to jupyterhub
-chown -R ${1}: /etc/letsencrypt/live/ /etc/letsencrypt/archive/
-# chmod 600 /etc/letsencrypt/archive/$HOST/privkey1.pem
+chown -R ${USERNAME}: /etc/letsencrypt/live/ /etc/letsencrypt/archive/
 
 # create the conda environment
 cd aml-jupyterhub
-/home/${1}/miniconda/bin/conda env create -f env.yaml
+/home/${USERNAME}/miniconda/bin/conda env create -f env.yaml
 
 # write environment vars to .env file
 rm -f .env
@@ -55,16 +66,16 @@ echo "AAD_TENANT_ID=${4}" >> .env
 echo "AAD_CLIENT_ID=${5}" >> .env
 echo "AAD_CLIENT_SECRET=${6}" >> .env
 echo "HOST=$HOST" >> .env
-echo "JUPYTERHUB_ADMIN=${1}" >> .env
+echo "JUPYTERHUB_ADMIN=${USERNAME}" >> .env
 echo "JUPYTERHUB_CRYPT_KEY=$(openssl rand -hex 32)" >> .env
 echo "SSL_KEY=/etc/letsencrypt/live/$HOST/privkey.pem" >> .env
 echo "SSL_CERT=/etc/letsencrypt/live/$HOST/fullchain.pem" >> .env
 
 # clone the aml-jupyterhub repo and create the conda environment
-chown -R ${1}:${1} /home/${1}/miniconda
-chown -R ${1}:${1} /home/${1}/aml-jupyterhub
+chown -R ${USERNAME}:${USERNAME} /home/${USERNAME}/miniconda
+chown -R ${USERNAME}:${USERNAME} /home/${USERNAME}/aml-jupyterhub
 
 # download the run script, and save as run.sh
 wget ${8} -O run.sh
 # call the run script - run it as vm-admin-username
-sudo -u ${1} bash run.sh   # ${2} ${3} ${4} ${5} ${6} ${7}
+sudo -u ${USERNAME} bash run.sh
--- deployment_scripts/run.sh
+++ deployment_scripts/run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# This script will be run as the jupyterhub admin user.
+# First activate the conda environment, then set env vars, then run jupyterhub
+
 cd /home/${USER}/aml-jupyterhub
-echo "about to do conda init bash"
+
+# activate the conda environment
 /home/${USER}/miniconda/bin/conda init bash
 . /home/${USER}/miniconda/etc/profile.d/conda.sh
-echo $PATH > path3.txt
-
 conda activate azml
 
 # set environment variables needed by the spawner
@@ -13,9 +15,5 @@ set -o allexport
 source .env
 set +o allexport
 
-printenv > environment.txt
-
-echo $PATH > path2.txt
-echo `which python` >> path2.txt
-
+# Run jupyterhub
 nohup python -m jupyterhub -f deployments/azure_ad_auth_spawn_aml/jupyterhub_config.py >& jupyterout.txt &
